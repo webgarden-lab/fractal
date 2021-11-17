f4f97897d351bbfd626dc1143a962395b0352d6b
annuh <annevandevenis@gmail.com>
2021-11-17 10:22:30 +0100

    Support PHP 8.1

--- src/Scope.php
+++ src/Scope.php
@@ -295,7 +295,7 @@ class Scope implements \JsonSerializable
     /**
      * @return array
      */
-    public function jsonSerialize()
+    public function jsonSerialize(): mixed
     {
         return $this->toArray();
     }
